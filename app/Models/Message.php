f05af43b0ac617f10e5cdc400a263ab96e82fdca
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-26 04:20:30 +0900

    [add] model relation edit

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Thread;
 
 class Message extends Model
 {
-    use HasFactory;
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Message;
 
 class Thread extends Model
 {
-    use HasFactory;
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
