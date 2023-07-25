b3d2a8583bd0d10bf4a2101001514e00bc07c2e2
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-26 03:57:53 +0900

    [fix] add ;

--- database/migrations/2023_07_25_175549_create_threads_table.php
+++ database/migrations/2023_07_25_175549_create_threads_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
-            $table->string("title")
-            $table->string("overview")
+            $table->string("title");
+            $table->string("overview");
             $table->timestamps();
         });
     }
