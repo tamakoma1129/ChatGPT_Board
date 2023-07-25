06159bee4333b1bb4b98bb50c965cfcf5d0754bb
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-26 05:19:25 +0900

    [add] ThreadsSeeder edit

--- database/seeders/ThreadsTableSeeder.php
+++ database/seeders/ThreadsTableSeeder.php
@@ -14,6 +14,25 @@ class ThreadsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('threads')->insert([
+            [
+                'title' => 'サンプルスレッド1',
+                'overview' => 'これはサンプルのスレッド1です。',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'サンプルスレッド2',
+                'overview' => 'これはサンプルのスレッド2です。',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'サンプルスレッド3',
+                'overview' => 'これはサンプルのスレッド3です。',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
