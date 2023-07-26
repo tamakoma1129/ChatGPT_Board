f69255d7033d2304b9672bf5003778400989dca9
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 00:42:44 +0900

    [add] ThreadsDtailSeeder edit

--- database/seeders/ThreadsDetailSeeder.php
+++ database/seeders/ThreadsDetailSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ThreadsDetailSeeder extends Seeder
 {
@@ -14,6 +15,28 @@ class ThreadsDetailSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('messages')->insert([
+            [
+                'thread_id' => '1',
+                'message' => 'テストメッセージ1です',
+                'role' => 'User',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'thread_id' => '1',
+                'message' => 'テストメッセージ2です',
+                'role' => 'ChatGPT',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'thread_id' => '1',
+                'message' => 'テストメッセージ3です',
+                'role' => 'User',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
