6af7670f405582e2330cabe9b1c09bbda04954f7
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-26 11:06:46 +0900

    [fix] Thread index.blade.php

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <title>ChatGPT_Board</title>
-        <style>body {padding: 10px;}</style>
-    </head>
-    <body>
-        <h1>スレッド一覧</h1>
+@extends('layout')
 
-        @foreach ($threads as $thread)
-            <p>
-                {{ $thread->title}},
-                {{ $thread->overview }},
-                {{ $thread->timestamps }}
-            </p>
-        @endforeach
-    </body>
-</html>
+@section('content')
+    <h1>スレッド一覧</h1>
+
+    @foreach ($threads as $thread)
+        <p>
+            {{ $thread->title}},
+            {{ $thread->overview }},
+            {{ $thread->timestamps }}
+        </p>
+    @endforeach
+@endsection
