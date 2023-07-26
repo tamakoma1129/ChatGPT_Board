9627bf3b1682f405561a85a2209f0d5a899ed3b8
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-26 11:03:45 +0900

    [add] Thread layout

---
+++ resources/views/thread/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>ChatGPT_Board</title>
+        <style>body {padding: 10px;}</style>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
