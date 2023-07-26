f5330085db3764129dc57d24374e2beb739d7a13
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 02:08:59 +0900

    [add] storeRouting

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ThreadController;
 
 Route::get("/threads", [ThreadController::class, "index"])->name("threads.list");
 Route::get("/thread/create", [ThreadController::class, "create"])->name("threads.create");
+Route::post("/thread/store", [ThreadController::class, "store"])->name("threads.store");
 Route::get("/thread/{id}",[ThreadController::class, "show"])->name("threads.detail");
 
 Route::get('/', function () {
