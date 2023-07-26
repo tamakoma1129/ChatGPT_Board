30eae400bf4602b2a2d58ae8326a08bbfd5af7c9
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 01:33:11 +0900

    [add] create rooting

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ThreadController;
 
 Route::get("/threads", [ThreadController::class, "index"])->name("threads.list");
+Route::get("/thread/create", [ThreadController::class, "create"])->name("threads.create");
 Route::get("/thread/{id}",[ThreadController::class, "show"])->name("threads.detail");
 
 Route::get('/', function () {
