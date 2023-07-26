a53ce8d37080ba25f8ffea714d345f4549af1e68
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 00:56:19 +0900

    [fix] indexView Tableize

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -3,11 +3,22 @@
 @section('content')
     <h1>スレッド一覧</h1>
 
-    @foreach ($threads as $thread)
-        <p>
-            {{ $thread->title}},
-            {{ $thread->overview }},
-            {{ $thread->timestamps }}
-        </p>
-    @endforeach
+    <table class='table table-striped table-hover'>
+        <tr>
+            <th>タイトル</th>
+            <th>概要</th>
+            <th>作成日時</th>
+        </tr>
+        @foreach ($threads as $thread)
+            <tr>
+                <td>
+                    <a href="{{ route('thread.show', ['id' => $thread->id]) }}">
+                        {{ $thread->title }}
+                    </a>
+                </td>
+                <td>{{ $thread->overview }}</td>
+                <td>{{ $thread->created_at }}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
