09148ddee4c7c7762f5196d5d7434a407875f14f
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 03:10:53 +0900

    [add] showView edit

--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -7,5 +7,21 @@
         <p>{{ $message->role }}</p>
         <p>{{ $message->message }}</p>
         <p>{{ $message->created_at }}</p>
+        <br>
     @endforeach
+
+    <form method="POST" action="{{ route('messages.store') }}">
+        @csrf
+        <input type="hidden" name="thread_id" value="{{ $thread->id }}">
+        <div>
+            <label for="role">役割:</label>
+            <input type="radio" id="role" name="role" value="User">User
+            <input type="radio" id="role" name="role" value="ChatGPT">ChatGPT
+        </div>
+        <div>
+            <label for="message">メッセージ:</label>
+            <input type="text" id="message" name="message">
+        </div>
+        <button type="submit">メッセージを作成</button>
+    <a href={{ route('threads.list') }} class='btn btn-outline-primary'>記事一覧へ戻る</a>
 @endsection
