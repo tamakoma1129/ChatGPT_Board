57e59c0886409c76548bb5d04627de295726efee
tamakoma <93993709+tamakoma1129@users.noreply.github.com>
2023-07-27 02:02:24 +0900

    [add] createView

---
+++ resources/views/thread/create.blade.php
@@ -0,0 +1,17 @@
+@extends('thread.layout')
+
+@section('content')
+    <h1>新しい記事の作成</h1>
+    <form method="POST" action="{{ route('threads.store') }}">
+        @csrf
+        <div>
+            <label for="title">タイトル:</label>
+            <input type="text" id="title" name="title">
+        </div>
+        <div>
+            <label for="overview">概要:</label>
+            <input type="text" id="overview" name="overview">
+        </div>
+        <button type="submit">記事を作成</button>
+    </form>
+@endsection
